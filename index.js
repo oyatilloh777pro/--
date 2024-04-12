b973914c02d24c730c0d992072e7b5240a91f94e
Oyatilloh <oyatillohqobiljonov81@gmail.com>
2024-04-12 11:33:17 +0500

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+let ism = prompt("Ismingiz nima: ");
+let Familya = prompt("Familyanfiz nima: ");
+let yosh = prompt("Yoshingiz nechida : ");
+let kasb = prompt("Qiziqan sohangiz : ");
+let manzil = prompt("Manzilingiz : ");
+alert ( "ism " + yosh - " + Familya + "+ Familya -"  + yosh + " yosh - " + kasb + " kasb " + manzil + " manzil" );
